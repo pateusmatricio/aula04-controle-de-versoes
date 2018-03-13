b044e7c98122b2a490af9573806dbc65519b6b0e
Mateus Patrício De Souza Santos <mateussantos@A307-39L.imd.local>
2018-03-13 18:08:46 -0300

    ta bugado

--- src/funcoes.cpp
+++ src/funcoes.cpp
@@ -11,23 +11,23 @@ float milionario(float salario, float despesas){
 void numerosRepetidos(int * numeros){
 	int i, j, repetido = 1, k = 0;
 	int numerosUsados[10];
+
+	for(i = 0; i < 10; i++){
+		numerosUsados[i] = 1;
+	}
 	
 	for(i = 0; i < 10; i++){
 		for(j = 0; j < 10; j++){
 			if(numeros[i] == numeros[j] && j != i){
-				repetido++;
+				numerosUsados[i] += 1;
+			} else {
+				numerosUsados[j] = 0;
 			}
 		}
-		numerosUsados[k] = numeros[i];
-		k++;
-		
-		for(j = 0; j < 10; j++){
-			if(numeros[i] == numeros[j] && j != i){
-				repetido++;
-			}
-		}
-		cout << "o numero " << numeros[i] << " foi digitado " << repetido << " vezes!" << endl;
-		repetido = 1;
+	}
+
+	for(i = 0; i < 10; i++) {
+		cout << numeros[i] << " aconteceu " << numerosUsados[i] << " vezes." << endl;
 	}
 }
 
